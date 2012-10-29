56fc65e026910c0d7c719ddbb26df13ee0243195
Jonathan Sebastian <5539299335@itelcel.com>
2012-10-29 08:22:57 -0600

    wip

--- ODE_2/Console2/OT2CCLN_MAIN.f90
+++ ODE_2/Console2/OT2CCLN_MAIN.f90
@@ -1,8 +1,32 @@
+
  real*16 function f1(x1,x2,x3,t) 
  IMPLICIT NONE
  real*16 x1,x2,x3,t
  real*16 xr 
-xr=-(t+1)*x1+x2
+ 
+ integer*4 solving_set
+common /solver_data/solving_set
+xr=0;
+ if (solving_set .eq. 1) then 
+xr=x1*x1
+end if
+
+ if (solving_set .eq. 2) then 
+xr=-(t+1.0Q0)*x1+x2
+end if
+
+ if (solving_set .eq. 3) then 
+xr=-x1-2.0Q0*x2+t*t
+end if
+
+ if (solving_set .eq. 4) then 
+xr=100.0Q0*(sin(t)-x1)
+end if
+
+ if (solving_set .eq. 5) then 
+xr=x2
+end if
+
 !xr=2*x1+4*x2+exp(4*t)
 f1=xr
 end function
@@ -11,7 +35,20 @@ end function
  IMPLICIT NONE
  real*16 x1,x2,x3,t
  real*16 xr 
-xr=-1*x1*cos(t)+x3
+ 
+ integer*4 solving_set
+common /solver_data/solving_set
+xr=0;
+ if (solving_set .eq. 2) then 
+xr=-x1*cos(t)+x3
+end if
+ if (solving_set .eq. 3) then 
+xr=1.0Q0/2.0Q0*x1+2.0Q0*x2+t
+end if
+
+ if (solving_set .eq. 5) then 
+xr=-1.001Q0*x2-1.0Q0*x1
+end if
 !xr=x1+2*x2;
 f2=xr
 end function
@@ -20,7 +57,13 @@ end function
  IMPLICIT NONE
  real*16 x1,x2,x3,t
  real*16 xr 
-xr=t*t +x1*(x1*sin(x1+t)+(t*t-1))
+ integer*4 solving_set
+common /solver_data/solving_set
+ xr=0;
+ 
+  if (solving_set .eq. 2) then 
+xr=t*t +x1*(x1*sin(x1+t)+(t*t-1.0Q0))
+end if
 f3=xr
 end function
 
@@ -67,24 +110,33 @@ real*16, INTENT(IN):: t,h
 
 end subroutine
 
-subroutine solve_euler_mod(x_1_i, x_2_i, x_3_i,t,h)   
+subroutine solve_euler_modified(x_1_i, x_2_i, x_3_i,t,h)   
 IMPLICIT NONE
  real*16 f1,f2,f3
 real*16, INTENT(INOUT)::  x_1_i, x_2_i, x_3_i
 real*16 x_1_o, x_2_o, x_3_o
 real*16 x_1_t1, x_2_t1, x_3_t1
+real*16 x_1_t2, x_2_t2, x_3_t2
 real*16, INTENT(IN):: t,h
- x_1_t1=f1(x_1_i,x_2_i,x_3_i,t);
- x_2_t1=f2(x_1_i,x_2_i,x_3_i,t);
-  x_3_t1=f3(x_1_i,x_2_i,x_3_i,t);
+ x_1_t1=h*f1(x_1_i,x_2_i,x_3_i,t);
+ x_2_t1=h*f2(x_1_i,x_2_i,x_3_i,t);
+  x_3_t1=h*f3(x_1_i,x_2_i,x_3_i,t);
 
- x_1_o=1.0Q0/2.0Q0*h*(x_1_t1+f1(x_1_i+x_1_t1*h ,x_2_i+x_2_t1*h,x_3_i+x_3_t1*h,t+h)*h)
+ x_1_t2=h*f1(x_1_i+x_1_t1,x_2_i,x_3_i,t+h);
+ x_2_t2=h*f2(x_1_i,x_2_i+x_2_t1,x_3_i,t+h);
+  x_3_t2=h*f3(x_1_i,x_2_i,x_3_i+x_3_t1,t+h);
+
+ x_1_o=1.0Q0/2.0Q0*(x_1_t1+x_1_t2)
+ 
+ !1.0Q0/2.0Q0*h*(x_1_t1+f1(x_1_i+x_1_t1*h ,x_2_i+x_2_t1*h,x_3_i+x_3_t1*h,t+h)*h)
  
 
- x_2_o=1.0Q0/2.0Q0*h*(x_2_t1+f2(x_1_i+x_1_t1*h ,x_2_i+x_2_t1*h,x_3_i+x_3_t1*h,t+h)*h)
+ x_2_o=1.0Q0/2.0Q0*(x_2_t1+x_2_t2)
+ ! 1.0Q0/2.0Q0*h*(x_2_t1+f2(x_1_i+x_1_t1*h ,x_2_i+x_2_t1*h,x_3_i+x_3_t1*h,t+h)*h)
  
 
- x_3_o=1.0Q0/2.0Q0*h*(x_3_t1+f3(x_1_i+x_1_t1*h ,x_2_i+x_2_t1*h,x_3_i+x_3_t1*h,t+h)*h)
+ x_3_o=1.0Q0/2.0Q0*(x_3_t1+x_3_t2)
+ !1.0Q0/2.0Q0*h*(x_3_t1+f3(x_1_i+x_1_t1*h ,x_2_i+x_2_t1*h,x_3_i+x_3_t1*h,t+h)*h)
  
  
 
@@ -503,14 +555,22 @@ end do
 end subroutine
 
 
+subroutine set_ini(x_1_i, x_2_i, x_3_i,x_1, x_2, x_3)
+IMPLICIT NONE
 
+real*16, INTENT(INOUT)::  x_1_i, x_2_i, x_3_i
+real*16, INTENT(INOUT)::  x_1, x_2, x_3
+x_1_i=x_1
+x_2_i=x_2
+x_3_i=x_3
+end subroutine
 
 
 subroutine writer(i,x_1_i, x_2_i, x_3_i,t)
 IMPLICIT NONE
 10 format ((f16.6),',',(f16.6),',',(f16.6),',',(f16.6))
  real*16 x_1_i, x_2_i, x_3_i, t
- integer*8 i;
+ integer*8 i,titer,ttime;
 if (mod(i,500) .eq. 0) then
 write(100,10)t,x_1_i,x_2_i,x_3_i
 end if 
@@ -525,61 +585,203 @@ PROGRAM ODE
  real*16 x_1, x_2, x_3
 
 integer*8 i
+integer*4 solving_set
+common /solver_data/solving_set
 
-
-
+solving_set=5
 t=0
+
+x_1=0.0Q0  !1.1
+x_2=0.0Q0 !2.2
+x_3=0.0Q0
+ttime=0.0Q0
+h=.00001Q0
+if (solving_set==2) then
 x_1=1.1Q0  !1.1
 x_2=2.2Q0 !2.2
 x_3=3.3Q0
+ttime=10.0Q0
 
 
-x_1_i(4)=x_1
-x_2_i(4)=x_2
-x_3_i(4)=x_3
+end if
 
-h=.00001Q0
+if (solving_set==1) then
+x_1=0.5Q0  !1.1
+ttime=1.0Q0
+end if
 
-open (100, FILE='datos.csv')
-do i=1,1000000
+
+if (solving_set==3) then
+x_1=-2.0Q0  !1.1
+x_2=1.0Q0  !1.1
+ttime=2.0Q0
+end if
+if (solving_set==4) then
+x_1=0.0Q0  !1.1
+ttime=4.0Q0
+end if
+if (solving_set==5) then
+x_1=1.0Q0  !1.1
+x_2=-1.0Q0  !1.1
+ttime=3.0Q0
+end if
+
+
+titer=ttime/h;
+
+Write(*,*) 'Solving Euler'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_euler.csv')
+do i=1,titer
   t=h*(i-1);
-!  call solve_euler(x_1_i, x_2_i, x_3_i,t,h)
-  !call solve_rk4(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
-call solve_euler_mod(x_1_i(4), x_2_i(4), x_3_i(4),t,h)    !!verificarlo
-! call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
-! call solve_euler_trap(x_1_i, x_2_i, x_3_i,t,h)
- !call solve_trap(x_1_i(4), x_2_i(4), x_3_i(4),t,h) 
-! call solve_rkm(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+  call solve_euler(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
  call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
 end do
+close (100,STATUS='KEEP')
 
+Write(*,*) 'Solving RK4'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_rk4.csv')
+do i=1,titer
+  t=h*(i-1);
+call solve_rk4(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+ call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
+
+
+Write(*,*) 'Solving Euler Mod'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_euler_mod.csv')
+do i=1,titer
+  t=h*(i-1);
+call solve_euler_modified(x_1_i(4), x_2_i(4), x_3_i(4),t,h)   
+ call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
+
+Write(*,*) 'Solving Euler Implicit'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_euler_implicit.csv')
+do i=1,titer
+  t=h*(i-1);
+call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+ call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
+
+Write(*,*) 'Solving Euler Pre- Trap Corr'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_euler_trap.csv')
+do i=1,titer
+  t=h*(i-1);
+ call solve_euler_trap(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+ call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
+
+Write(*,*) 'Solving Trap'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_trap.csv')
+do i=1,titer
+  t=h*(i-1);
+call solve_trap(x_1_i(4), x_2_i(4), x_3_i(4),t,h) 
+ call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
+
+Write(*,*) 'Solving RKM'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_rkm.csv')
+do i=1,titer
+  t=h*(i-1);
+call solve_rkm(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+ call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
+
+
+
+Write(*,*) 'Solving AM4'
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_am4.csv')
   t=h*(0);
-  call rotate(x_1_i, x_2_i, x_3_i,4)
+call rotate(x_1_i, x_2_i, x_3_i,4)
 call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
-
-  t=h*(1);
-  call rotate(x_1_i, x_2_i, x_3_i,4)
+t=h*(1);
+call rotate(x_1_i, x_2_i, x_3_i,4)
 call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
-
-  t=h*(2);
-  call rotate(x_1_i, x_2_i, x_3_i,4)
+t=h*(2);
+call rotate(x_1_i, x_2_i, x_3_i,4)
 call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+do i=4,titer
+  t=h*(i-1);
+call solve_am4(x_1_i, x_2_i, x_3_i,t,h,4) 
+ !call solve_gear1(x_1_i(4), x_2_i(4), x_3_i(4),t,h) 
+! call solve_gear2(x_1_i, x_2_i, x_3_i,t,h,4) 
+call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
 
 
-do i=4,1000000
+
+
+Write(*,*) 'Solving Gear1 =Euler Impl '
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_Gear1.csv')
+  t=h*(0);
+call rotate(x_1_i, x_2_i, x_3_i,4)
+call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+t=h*(1);
+call rotate(x_1_i, x_2_i, x_3_i,4)
+call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+t=h*(2);
+call rotate(x_1_i, x_2_i, x_3_i,4)
+call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+do i=4,titer
+  t=h*(i-1);
+!call solve_am4(x_1_i, x_2_i, x_3_i,t,h,4) 
+ call solve_gear1(x_1_i(4), x_2_i(4), x_3_i(4),t,h) 
+! call solve_gear2(x_1_i, x_2_i, x_3_i,t,h,4) 
+call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+end do
+close (100,STATUS='KEEP')
+
+Write(*,*) 'Solving Gear2  '
+call set_ini(x_1_i(4),x_2_i(4),x_3_i(4),x_1,x_2,x_3)
+open (100, FILE='datos_Gear2.csv')
+  t=h*(0);
+call rotate(x_1_i, x_2_i, x_3_i,4)
+call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+t=h*(1);
+call rotate(x_1_i, x_2_i, x_3_i,4)
+call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+t=h*(2);
+call rotate(x_1_i, x_2_i, x_3_i,4)
+call solve_euler_implicit(x_1_i(4), x_2_i(4), x_3_i(4),t,h)
+do i=4,titer
   t=h*(i-1);
 !call solve_am4(x_1_i, x_2_i, x_3_i,t,h,4) 
  !call solve_gear1(x_1_i(4), x_2_i(4), x_3_i(4),t,h) 
-! call solve_gear2(x_1_i, x_2_i, x_3_i,t,h,4) 
-!call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
+ !call solve_gear2(x_1_i, x_2_i, x_3_i,t,h,4) 
+call writer(i,x_1_i(4), x_2_i(4), x_3_i(4),t)
 end do
-
-
-
-
-
-
 close (100,STATUS='KEEP')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 WRITE (*,*)'DONE'
 !pause
 end program ODE
